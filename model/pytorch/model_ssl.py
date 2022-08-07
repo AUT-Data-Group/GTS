3c864d7e83c680965fb2f68ea80be04081e7c0d9
moghadas76 <s.m.moghadas2012@gmail.com>
2022-08-07 12:24:17 +0430

    fix

--- model/pytorch/model_ssl.py
+++ model/pytorch/model_ssl.py
@@ -227,6 +227,33 @@ class ViViTSSL(nn.Module):
         z = x[:, 0]
         return z
 
+    def random_vertex_masking(self, x, mask_ratio):
+        """
+        Perform per-sample random masking by per-sample shuffling.
+        Per-sample shuffling is done by argsort random noise.
+        x: [N, L, D], sequence
+        """
+        N, L, D, _ = x.shape  # batch, length, dim
+        len_keep = int(D * (1 - mask_ratio))
+        
+        noise = torch.rand(N, D, device=x.device)  # noise in [0, 1]
+        
+        # sort noise for each sample
+        ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+        # keep the first subset
+        ids_keep = ids_shuffle[:,:, :len_keep,:]
+        x_masked = torch.gather(x, dim=2, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+        # generate the binary mask: 0 is keep, 1 is remove
+        mask = torch.ones([N, L, D], device=x.device)
+        mask[:,:, :len_keep] = 0
+        # unshuffle to get the binary mask
+        mask = torch.gather(mask, dim=2, index=ids_restore)
+
+        return x_masked, mask, ids_restore
+
     def random_masking(self, x, mask_ratio):
         """
         Perform per-sample random masking by per-sample shuffling.
