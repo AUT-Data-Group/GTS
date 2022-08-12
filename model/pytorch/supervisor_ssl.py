Fix: spatial masking

model/pytorch/supervisor_ssl.py
@@ -15,8 +15,9 @@ from ray.tune.schedulers import ASHAScheduler
 from ray.tune.integration.wandb import wandb_mixin, WandbLogger
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-wandb.init(project="transformer", entity="aufl")
+DEBUG = os.environ.get("DEBUG")
+if not DEBUG:
+    wandb.init(project="transformer", entity="aufl")
 
 class GTSSupervisor:
     def __init__(self, save_adj_name, temperature, **kwargs):