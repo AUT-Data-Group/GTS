Fix: spatial masking

model/pytorch/model_ssl.py
@@ -162,7 +162,10 @@ class ViViTSSL(nn.Module):
         num_patches = num_patches + 1
 
         self.pos_embed = nn.Parameter(torch.zeros(1,num_patches,embed_dims))
-        self.time_embed = nn.Parameter(torch.zeros(1,int((1 - mask_ratio)*num_frames),embed_dims))
+        if mode == "temporal":
+            self.time_embed = nn.Parameter(torch.zeros(1,int((1 - mask_ratio)*num_frames),embed_dims))
+        else:
+            self.time_embed = nn.Parameter(torch.zeros(1,num_frames,embed_dims))
         self.drop_after_pos = nn.Dropout(p=dropout_p)
         self.drop_after_time = nn.Dropout(p=dropout_p)
         self.fc = nn.Linear(768, 414)