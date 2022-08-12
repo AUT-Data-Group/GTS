Fix: spatial masking

model/pytorch/model_ssl.py
@@ -251,7 +251,7 @@ class ViViTSSL(nn.Module):
         ids_restore = torch.argsort(ids_shuffle, dim=1)
 
         # keep the first subset
-        ids_keep = ids_shuffle[:,:, :len_keep,:]
+        ids_keep = ids_shuffle[:,:len_keep]
         x_masked = torch.gather(x, dim=2, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
 
         # generate the binary mask: 0 is keep, 1 is remove